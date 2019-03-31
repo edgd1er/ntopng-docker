17e6113e4184867ed5cd2126ac92565b240647e3
edgd1er <edgd1er@hotmail.com>
2019-03-31 13:10:31 +0200

    add failure info

--- build.sh
+++ build.sh
@@ -4,7 +4,12 @@ mach=$(uname -m)
 [[ ${mach} =~ "arm" ]] && docker build -f Dockerfile.armhf -t edgd1er/ntopng-docker:armhf .
 if [[ ${mach} =~ "x86" ]]; then 
 	docker build -f Dockerfile -t edgd1er/ntopng-docker:amd64 .
+	ret=$?
+	[[ ${ret} != "0" ]] && echo "\n error while building amd64 image" && exit -1
+
 	docker build -f Dockerfile.armhf -t edgd1er/ntopng-docker:armhf .
+	ret=$?
+	[[ ${ret} != "0" ]] && echo "\n error while building amd64 image" && exit -1
 fi
 
 docker manifest create edgd1er/ntopng-docker edgd1er/ntopng-docker:armhf edgd1er/ntopng-docker:amd64
